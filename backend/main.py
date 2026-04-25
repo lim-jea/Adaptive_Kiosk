5ed3c885c619ffd29ca2b99055cf77b824a89173
kyumine <qkrrbals1014@gmail.com>
2026-04-25 16:34:51 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -135,20 +135,10 @@ app = FastAPI(
 )
 
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:4000",
-    "http://localhost:5000",
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
