f2ef93c2a3351672975943c4acc0933bdd3ef627
kyumine <qkrrbals1014@gmail.com>
2026-04-25 15:59:48 +0900

    4.25_ver2

--- backend/main.py
+++ backend/main.py
@@ -138,6 +138,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://localhost:4000",
     "http://localhost:5000",
     "http://localhost:5173",
     "http://localhost:8000",
